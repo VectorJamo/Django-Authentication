b78131eb4d7dabc4c428b9c42c0e270a95a1a503
unknown <nsuraj552@gmail.com>
2024-07-30 12:47:11 -0600

    User Registration

---
+++ user_auth/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from django import forms
+
+class UserRegistraionForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import forms
+
 # Create your views here.
 def homepage(request):
     return render(request, 'user_auth/homepage.html', {'title': 'Homepage'})
 
 def register_user(request):
-    pass
+    # TODO: Handle request coming from custom HTML form.
+    if request.method == 'POST':
+        print('POST request arrived!')
+        
+    form = forms.UserRegistraionForm()
+
+    return render(request, 'user_auth/register.html', {'form': form})
