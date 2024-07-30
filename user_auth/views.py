afb9bf3990312a1a08f6b9f8a777c95ae6c75567
unknown <nsuraj552@gmail.com>
2024-07-29 20:35:25 -0600

    Created the base template

--- user_auth/views.py
+++ user_auth/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'user_auth/homepage.html', {'title': 'Homepage'})
+
+def register_user(request):
+    pass
