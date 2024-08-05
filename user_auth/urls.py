baf04009c4fff0dc76c4f842c9f5f446722d0f17
unknown <nsuraj552@gmail.com>
2024-08-05 12:56:32 -0600

    Finished authentication

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('register/', views.register_user, name='register-user'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='user_auth/login.html'), name='login-user'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user_auth/logout.html'), name='logout-user'),
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,17 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from . import forms
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required
 def homepage(request):
     return render(request, 'user_auth/homepage.html', {'title': 'Homepage'})
 
 def register_user(request):
     # TODO: Handle request coming from custom HTML form.
-    if request.method == 'POST':
-        print('POST request arrived!')
-        
-    form = forms.UserRegistraionForm()
+    if request.user.is_authenticated:
+        return redirect('homepage')
+    else:
+        if request.method == 'POST':
+            print(request.POST)
+            form = forms.UserRegistraionForm(request.POST)
+            if form.is_valid():
+                print('Valid form information!')
+                form.save()
 
-    return render(request, 'user_auth/register.html', {'form': form})
+                return redirect('login-user')
+            else:
+                print('Invalid form information')
+                errors = form.errors
+                for field, error_list in errors.items():
+                    for error in error_list:
+                        print(f"Error in {field}: {error}")
+            
+        form = forms.UserRegistraionForm()
+
+        return render(request, 'user_auth/register.html', {'form': form})
